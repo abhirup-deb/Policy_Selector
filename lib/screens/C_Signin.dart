96a326c6751e8cbee42abed0edb6ac9c8c13b4e3
abhirup-deb <debabhirup1999@gmail.com>
2019-12-28 20:40:48 +0530

    Commissioner Sign in Screen Modified

--- lib/screens/C_Signin.dart
+++ lib/screens/C_Signin.dart
@@ -10,7 +10,37 @@ class _C_SigninState extends State<C_Signin>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hi from C'),
+      appBar: AppBar(title: Text('Commissioner'),centerTitle: true,),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          height: 450.0,
+          width: 350.0,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(27.0),color: Colors.black12),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 70.0,width:240.0,
+                    child: TextField(style: TextStyle(color: Colors.white70),textAlign: TextAlign.center,decoration: InputDecoration(
+                        hintText: 'Enter Your Name',hintStyle: TextStyle(color: Colors.blueGrey)
+                    ),)),
+                SizedBox(
+                  height: 3.0,
+                ),
+                Container(height: 70.0,width:240.0,
+                    child: TextField(style: TextStyle(color: Colors.white70),textAlign: TextAlign.center,decoration: InputDecoration(
+                        hintText: 'Enter Your Aadhar No.',hintStyle: TextStyle(color: Colors.blueGrey)
+                    ),)),
+                SizedBox(height: 4.0,),
+                RaisedButton(onPressed: (){},
+                  child: Text('Login',style: TextStyle(fontSize: 20.0),),color: Colors.orangeAccent,elevation: 7.0,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(38.0),),),
+
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
