d45af17a43295cd51794de49be47324b7861c8d1
abhirup-deb <debabhirup1999@gmail.com>
2019-12-28 19:48:29 +0530

    User Sign in Screen Modified

--- lib/screens/User_Signin.dart
+++ lib/screens/User_Signin.dart
@@ -10,7 +10,15 @@ class _User_SigninState extends State<User_Signin>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hi from U'),
+      appBar: AppBar(title: Text('User'),centerTitle: true,),
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          height: 450.0,
+          width: 350.0,
+          decoration: BoxDecoration(borderRadius: BorderRadius.circular(27.0),color: Colors.black12),
+        ),
+      ),
     );
   }
 }
