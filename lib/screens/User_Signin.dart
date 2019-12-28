a6588be18d71c597ef90ca9175ac2e86d2a856e7
abhirup-deb <debabhirup1999@gmail.com>
2019-12-28 20:26:49 +0530

    User Sign in UI completed

--- lib/screens/User_Signin.dart
+++ lib/screens/User_Signin.dart
@@ -17,6 +17,28 @@ class _User_SigninState extends State<User_Signin>{
           height: 450.0,
           width: 350.0,
           decoration: BoxDecoration(borderRadius: BorderRadius.circular(27.0),color: Colors.black12),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(height: 70.0,width:240.0,
+                    child: TextField(style: TextStyle(color: Colors.white70),textAlign: TextAlign.center,decoration: InputDecoration(
+                      hintText: 'Enter Your Name',hintStyle: TextStyle(color: Colors.blueGrey)
+                    ),)),
+                SizedBox(
+                  height: 3.0,
+                ),
+                Container(height: 70.0,width:240.0,
+                    child: TextField(style: TextStyle(color: Colors.white70),textAlign: TextAlign.center,decoration: InputDecoration(
+                        hintText: 'Enter Your Aadhar No.',hintStyle: TextStyle(color: Colors.blueGrey)
+                    ),)),
+                SizedBox(height: 4.0,),
+                RaisedButton(onPressed: (){},
+                  child: Text('Login',style: TextStyle(fontSize: 20.0),),color: Colors.orangeAccent,elevation: 7.0,shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(38.0),),),
+
+              ],
+            ),
+          ),
         ),
       ),
     );
